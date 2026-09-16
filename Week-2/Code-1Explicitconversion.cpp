5e69a4e01f29289c5f4c6c553770ddc5d593a901
AASTIK CHHIBBAR <aastikc2007@gmail.com>
2026-09-16 16:21:36 +0530

    Add Code-1 of Week-2

---
+++ Week-2/Code-1Explicitconversion.cpp
@@ -0,0 +1,17 @@
+// Explicit Conversion 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int var1=100;
+    float var2=345.90;
+
+    cout << var1 << endl;
+    cout << var2 << endl;
+
+    cout << float(var1) << endl;
+    cout << int(var2);
+
+    return 0;
+}
